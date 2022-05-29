42d08a92589f8aa751cad83cafd69ab9dda8a594
kapra99 <sashko_99@mail.ru>
2022-05-29 19:10:19 +0300

    Added JWT Token Based Authentication & Authorization

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "kapra-secret-key"
+};
--- server.js
+++ server.js
@@ -16,11 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
 
 const db = require("./app/models");
+const Role = db.role;
 
 // db.sequelize.sync();
-// drop the table if it already exists
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Database with { force: true }');
+  initial();
 });
 
 // simple route
@@ -30,9 +31,28 @@ app.get("/", (req, res) => {
 
 require("./app/routes/carbrand.routes")(app);
 require("./app/routes/carmodel.routes")(app);
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
