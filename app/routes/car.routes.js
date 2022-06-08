1ed5cbaac7759d178747321ac2d6eb7bd299e7c0
kapra99 <sashko_99@mail.ru>
2022-06-08 22:23:50 +0300

    Adding update

--- app/routes/car.routes.js
+++ app/routes/car.routes.js
@@ -3,20 +3,14 @@ module.exports = app => {
 
   var router = require("express").Router();
 
-  // Create a new Tutorial
   router.post("/", cars.create);
 
-  // Retrieve all Tutorials
   router.get("/", cars.findAll);
 
-  // Retrieve all published cars
-  // router.get("/published", cars.findAllPublished);
 
-  // Retrieve a single Tutorial with id
   router.get("/:id", cars.findOne);
 
-  // Update a Tutorial with id
-  // router.put("/:id", cars.update);
+  router.put("/:id", cars.update);
 
   // Delete a Tutorial with id
   // router.delete("/:id", cars.delete);
